10bebe53c33e1d8e0f8d09e6384fd75da62025fb
kodevasse <kodevasse@outlook.com>
2022-09-21 20:29:18 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
+    exposeConfig: false,
+
+    injectPosition: 0,
+    viewer: true,
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+  runtimeConfig: {
+    private: {
+      stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+      db: process.env.DATABASE_URL,
+    },
+    public: {
+      appDomain: process.env.APP_DOMAIN,
+    },
+  },
+});
